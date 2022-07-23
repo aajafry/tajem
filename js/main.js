383de9a4cafb9162fdd41d1e4aecc7c24b9b28af
Al-Abed Jafry <aajafry2@users.noreply.github.com>
2022-07-23 21:02:43 +0600

    uploaded all file of tajem.

---
+++ js/main.js
@@ -0,0 +1,53 @@
+(function ($) {
+	"use strict";
+	
+	//sticky
+        $(".header-area").sticky({topSpacing:0});
+        //smoothScroll
+
+        smoothScroll.init({
+            speed: 1000,
+            easing: 'Linear',
+            offset: 0
+        });
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+        //YTPlayer call here.
+        $('.our-story-area').YTPlayer({
+            fitToBackground: true,
+            videoId: 'LSmgKRx5pBo'
+        });
+        //magnificPopup call here.
+        $('.video-play-icon').magnificPopup({
+            type: 'video'
+        });
+        //lightSlider
+        $(".testimonial-list").lightSlider({
+            item: 1,
+            //auto: true,
+            loop: true,
+            gallery: true,
+            thumbItem:5
+        });
+            
+
+        
+       
+        
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
